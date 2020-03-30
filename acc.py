1a572f3947f3f0088fb886c424724922941e8c0c
nk930439gj94 <nk930439gj94@gmail.com>
2020-03-31 02:17:01 +0800

    add acc.py

---
+++ acc.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+# import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+import time
+
+import sys
+from utility import *
+
+train_file_path = sys.argv[1]
+
+print("Reading data")
+train_x, train_y = readfile(train_file_path, True)
+print("Size of training data = {}".format(len(train_x)))
+val_x, val_y = readfile(valid_file_path, True)
+print("Size of validation data = {}".format(len(val_x)))
+
+train_set = ImgDataset(train_x, train_y, train_transform)
+train_val_x = np.concatenate((train_x, val_x), axis=0)
+train_val_y = np.concatenate((train_y, val_y), axis=0)
+train_val_set = ImgDataset(train_val_x, train_val_y, train_transform)
+
+train_val_loader = DataLoader(train_val_set, batch_size=128, shuffle=True)
+
+model = Classifier().cuda()
+model.load_state_dict( torch.load('model/cnn') )
+
+with torch.no_grad():
+	for i, data in enumerate(train_val_loader):
+		train_pred = model_best(data[0].cuda())
+		train_acc += np.sum(np.argmax(train_pred.cpu().data.numpy(), axis=1) == data[1].numpy())
+
+print( 'Train Acc: %3.6f' % train_acc/train_val_set.__len__() )
--- foodclassifier_test.py
+++ foodclassifier_test.py
@@ -30,14 +30,14 @@ model.load_state_dict( torch.load('model/cnn') )
 model.eval()
 prediction = []
 with torch.no_grad():
-    for i, data in enumerate(test_loader):
-        test_pred = model(data.cuda())
-        test_label = np.argmax(test_pred.cpu().data.numpy(), axis=1)
-        for y in test_label:
-            prediction.append(y)
+	for i, data in enumerate(test_loader):
+		test_pred = model(data.cuda())
+		test_label = np.argmax(test_pred.cpu().data.numpy(), axis=1)
+		for y in test_label:
+			prediction.append(y)
 
 #將結果寫入 csv 檔
 with open(out_file_path, 'w') as f:
-    f.write('Id,Category\n')
-    for i, y in  enumerate(prediction):
-        f.write('{},{}\n'.format(i, y))
+	f.write('Id,Category\n')
+	for i, y in  enumerate(prediction):
+		f.write('{},{}\n'.format(i, y))
