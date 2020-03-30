dde7ab2320198f78fa421fd74c1dd80d2952379c
nk930439gj94 <nk930439gj94@gmail.com>
2020-03-30 22:25:16 +0800

    fucking indent

--- utility.py
+++ utility.py
@@ -8,36 +8,36 @@ import pandas as pd
 from torch.utils.data import DataLoader, Dataset
 
 def readfile(path, label):
-    # label 是一個 boolean variable，代表需不需要回傳 y 值
-    image_dir = sorted(os.listdir(path))
-    x = np.zeros((len(image_dir), 128, 128, 3), dtype=np.uint8)
-    y = np.zeros((len(image_dir)), dtype=np.uint8)
-    for i, file in enumerate(image_dir):
-        img = cv2.imread(os.path.join(path, file))
-        x[i] = cv2.resize(img,(128, 128))
-        if label:
-          y[i] = int(file.split("_")[0])
-    if label:
-      return x, y
-    else:
-      return x
+	# label 是一個 boolean variable，代表需不需要回傳 y 值
+	image_dir = sorted(os.listdir(path))
+	x = np.zeros((len(image_dir), 128, 128, 3), dtype=np.uint8)
+	y = np.zeros((len(image_dir)), dtype=np.uint8)
+	for i, file in enumerate(image_dir):
+		img = cv2.imread(os.path.join(path, file))
+		x[i] = cv2.resize(img,(128, 128))
+		if label:
+			y[i] = int(file.split("_")[0])
+	if label:
+		return x, y
+	else:
+		return x
 
 class ImgDataset(Dataset):
-    def __init__(self, x, y=None, transform=None):
-        self.x = x
-        # label is required to be a LongTensor
-        self.y = y
-        if y is not None:
-            self.y = torch.LongTensor(y)
-        self.transform = transform
-    def __len__(self):
-        return len(self.x)
-    def __getitem__(self, index):
-        X = self.x[index]
-        if self.transform is not None:
-            X = self.transform(X)
-        if self.y is not None:
-            Y = self.y[index]
-            return X, Y
-        else:
-            return X
+	def __init__(self, x, y=None, transform=None):
+		self.x = x
+		# label is required to be a LongTensor
+		self.y = y
+		if y is not None:
+			self.y = torch.LongTensor(y)
+		self.transform = transform
+	def __len__(self):
+		return len(self.x)
+	def __getitem__(self, index):
+		X = self.x[index]
+		if self.transform is not None:
+			X = self.transform(X)
+		if self.y is not None:
+			Y = self.y[index]
+			return X, Y
+		else:
+			return X
